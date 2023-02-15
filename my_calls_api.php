f8ed2a444d283a64f14fdefdb5b40f1270eb04c5
Vladimir G <golovanov.v.mir@gmail.com>
2023-02-15 10:55:46 +0300

    В таблицу api_my_calls добавлен email пользователя

--- my_calls_api.php
+++ my_calls_api.php
@@ -18,6 +18,7 @@ $sql = "CREATE TABLE IF NOT EXISTS " . CALLS . " (
             id INT(11) NOT NULL AUTO_INCREMENT,
             date DATE NOT NULL,
             name VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL,
             count_in INT(11) NOT NULL,
             time_in INT(11) NOT NULL,
             count_out INT(11) NOT NULL,
@@ -28,8 +29,8 @@ $pdo->exec($sql);
 $dateNow = date('Y-m-d', $now);
 echo $dateNow;
 $sql = '';
-foreach ($Man->return() as $manager) {
-    $sqlCheck = "SELECT id FROM " . CALLS . " WHERE date='$dateNow' AND name='{$manager['FIO']}'";
+foreach ($Man->return() as $email => $manager) {
+    $sqlCheck = "SELECT id FROM " . CALLS . " WHERE date='$dateNow' AND email='$email'";
     $sth = $pdo->query($sqlCheck);
     $name = $manager['FIO'];
     $inCount = $manager[INCOMING]['count'];
@@ -39,11 +40,11 @@ foreach ($Man->return() as $manager) {
 
     if (!($sth->fetch()))
     {
-        $sql .= "INSERT INTO " . CALLS . " (`date`, `name`, `count_in`, `time_in`, `count_out`, `time_out`) VALUES ('$dateNow','$name',$inCount,$inTime,$outCount, $outTime);";
+        $sql .= "INSERT INTO " . CALLS . " (`date`, `name`, `email`, `count_in`, `time_in`, `count_out`, `time_out`) VALUES ('$dateNow','$name','$email',$inCount,$inTime,$outCount, $outTime);";
     }
     else
     {
-        $sql .= "UPDATE " . CALLS . " SET `count_in` = $inCount, `time_in` = $inTime, `count_out` = $outCount, `time_out` = $outTime WHERE date = '$dateNow' AND name = '$name';";
+        $sql .= "UPDATE " . CALLS . " SET `count_in` = $inCount, `time_in` = $inTime, `count_out` = $outCount, `time_out` = $outTime WHERE date = '$dateNow' AND email = '$email';";
     }
 }
 echo $sql;
