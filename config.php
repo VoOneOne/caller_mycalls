32b0608cc2d2f69745c77caa91289187e6666f06
Vladimir G <golovanov.v.mir@gmail.com>
2023-02-15 15:04:41 +0300

    Добавлены сотрудники в config

--- config.php
+++ config.php
@@ -7,7 +7,51 @@
         "getigezhev@worktruck.ru" => [
             'reg' => 'msk',
             'FIO' => 'Гетигежев Павел Юрьевич',
-        ]
+        ],
+        "mchuvilev@worktruck.ru" => [
+            'reg' => 'msk',
+            'FIO' => 'Чувилёв Михаил Александрович',
+        ],
+        "zozulya@worktruck.ru" => [
+            'reg' => 'nn',
+            'FIO' => 'Зозуля Сергей Александрович',
+        ],
+        "9788@worktruck.ru" => [
+            'reg' => 'msk',
+            'FIO' => 'Носовский Андрей Владимирович',
+        ],
+        "maa@worktruck.ru" => [
+            'reg' => 'nn',
+            'FIO' => 'Муравьев Андрей Александрович',
+        ],
+        "tarankov@worktruck.ru" => [
+            'reg' => 'nn',
+            'FIO' => 'Таранков Дмитрий Вадимович',
+        ],
+        "korolev@worktruck.ru" => [
+            'reg' => 'nn',
+            'FIO' => 'Королёв Алексей Николаевич',
+        ],
+        "kireev@worktruck.ru" => [
+            'reg' => 'nn',
+            'FIO' => 'Киреев Дмитрий Юрьевич',
+        ],
+        "i.muravev@worktruck.ru" => [
+            'reg' => 'nn',
+            'FIO' => 'Муравьёв Иван Андреевич',
+        ],
+        "polikanin@worktruck.ru" => [
+            'reg' => 'nn',
+            'FIO' => 'Румянцев Николай Владимирович',
+        ],
+        "hrwork@worktruck.ru" => [
+            'reg' => '',
+            'FIO' => 'Кадровик',
+        ],
+        "gorbas@worktruck.ru" => [
+            'reg' => 'msk',
+            'FIO' => 'Горбатовский Сергей Константинович',
+        ],
     ];
 
     const UNKNOWN = [
