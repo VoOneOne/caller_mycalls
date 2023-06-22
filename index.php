1b969987eab6c8b3fa1220a35b9c7229c8a075f9
Vladimir G <golovanov.v.mir@gmail.com>
2023-06-22 17:18:59 +0300

    добавлено отображение всех ошибок

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 const ABSPATH = __DIR__ . '/';
 include_once ABSPATH . 'config.php';
 include_once ABSPATH . 'dbconfig.php';
