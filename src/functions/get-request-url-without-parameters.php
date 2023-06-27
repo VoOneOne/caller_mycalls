7d6168cfcafdf098b65c142240420ac70296ebb7
Vladimir G <golovanov.v.mir@gmail.com>
2023-06-27 10:54:22 +0300

    var_dump

--- src/functions/get-request-url-without-parameters.php
+++ src/functions/get-request-url-without-parameters.php
@@ -1,5 +1,7 @@
 <?php
 function get_request_url_without_parameters(){
+
     $withoutParameters = substr($_SERVER['REQUEST_URI'],0, strpos($_SERVER['REQUEST_URI'],'?') ? : -1);
+    var_dump(get_site_url() . '/'. trim($withoutParameters, '/'));
     return get_site_url() . '/'. trim($withoutParameters, '/');
 }
