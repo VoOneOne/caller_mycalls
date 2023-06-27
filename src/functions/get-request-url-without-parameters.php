76e5682a5463638209740e11af11896cfd5242a6
Vladimir G <golovanov.v.mir@gmail.com>
2023-06-27 11:08:29 +0300

    исправлена ошибка в функции

--- src/functions/get-request-url-without-parameters.php
+++ src/functions/get-request-url-without-parameters.php
@@ -2,6 +2,5 @@
 function get_request_url_without_parameters(){
 
     $withoutParameters = substr($_SERVER['REQUEST_URI'],0, strpos($_SERVER['REQUEST_URI'],'?') ? : -1);
-    var_dump(get_site_url() . '/'. trim($withoutParameters, '/'));
     return get_site_url() . '/'. trim($withoutParameters, '/');
 }
